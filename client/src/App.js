d596b1e770a9ea5f9cedc2f0c882fc661a132abb
TTanev <tomatanev@gmail.com>
2021-03-15 17:31:23 +0200

    extract components from header

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header"
+import style from "./App.module.css"
 
 function App() {
+  console.log(style.tomapp)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.tomapp}>
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
